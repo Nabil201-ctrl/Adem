b6c69534e877e63096de8e4c479621976f921810
Nabil201-ctrl <abubakar.nabil.210@gmail.com>
2025-07-03 22:19:22 +0100

    updated the urls

--- login-form/utils.js
+++ login-form/utils.js
@@ -1,5 +1,5 @@
 // utils.js
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://adem-backend.onrender.com/api';
 export const LOGIN_PATH = '/login-form/index.html';
 export const BASE_URL = '';
 
